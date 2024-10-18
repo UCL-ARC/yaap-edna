b7cf2ae53fe3572fbf52d44843efee288629d8c9
Rob Vickerstaff <r.vickerstaff@ucl.ac.uk>
2024-10-18 15:30:43 +0100

    make test output more readable, added alternative yaap qsub script

--- test.sh
+++ test.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-set -eu
+#
+# test that all of the tools can print their help message
+#
 
+#activate the conda environment
 #not sure why conda init is not able to setup conda properly on myriad
 #but this method is a workaround
 source "/lustre/home/ccaervi/miniforge3/etc/profile.d/conda.sh"
-
 conda activate yaap
-cutadapt -h
-vsearch -h
-seqkit -h
-usearch
-pear -h
+
+test_command() {
+    CMD=$1
+    echo
+    echo " ==== ${CMD} ==== "
+    if [[ "${CMD}" != "usearch" ]] ; then
+        CMD="${CMD} -h"
+    fi
+    ${CMD} | head -n 6
+}
+
+for CMD in \
+    cutadapt \
+    vsearch \
+    seqkit \
+    usearch \
+    pear ;
+do
+    test_command $CMD
+done
