52589a2dcb6a53457cf5abe6dde38ed7d4f6f7da
Rob Vickerstaff <r.vickerstaff@ucl.ac.uk>
2024-11-25 10:29:43 +0000

    expand test script to create workspace folder and remove space from test data folder

--- test.sh
+++ test.sh
@@ -4,10 +4,12 @@
 # test that all of the tools can print their help message
 #
 
+set -eu
+
 #activate the conda environment
 #not sure why conda init is not able to setup conda properly on myriad
 #but this method is a workaround
-source "/lustre/home/ccaervi/miniforge3/etc/profile.d/conda.sh"
+source "/lustre/home/$(whoami)/miniforge3/etc/profile.d/conda.sh"
 conda activate yaap
 
 test_command() {
@@ -30,5 +32,13 @@ do
     test_command $CMD
 done
 
+#create a workspace folder and change into it
+export WORKSPACE_DIR=~/littlefair_workspace
+
+mkdir -p ${WORKSPACE_DIR} && cd ${WORKSPACE_DIR}
+
 #copy across test data
 rsync -a ccaervi@rdp-ssh.arc.ucl.ac.uk:/rdss/rd01/ritd-ag-project-rd01t5-jlitt26/test* .
+
+#remove space from test folder name
+mv ./test\ _for_Rob_ARCs/ ./test_for_Rob_ARCs/
